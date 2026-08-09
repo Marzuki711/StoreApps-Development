78b4df5a334d89864bbafc6b69c532e16a64477e
Marzuki711 <113434411+Marzuki711@users.noreply.github.com>
2026-08-09 14:57:54 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
-/*************************************************
+/*
  * Manual OT Claim System
  * config.js
  * Version : V2.0 (GitHub Pages)
- *************************************************/
+ */
 
 const CONFIG = {
 
@@ -10,8 +10,16 @@ const CONFIG = {
     // Google Apps Script Web App URL
     // ==========================================
 
+    // KEEP THIS URL FOR MANUAL OT / EXISTING API
     WEB_APP_URL:
-    "https://script.google.com/macros/s/AKfycbwgxK9FiVbmPBFHpvc3MgdEbpR5-BdsQ7x34oSjidnKGhmQoGBrPiwLFVhoUcJrx5AzVA/exec",
+        "https://script.google.com/macros/s/AKfycbwgxK9FiVbmPBFHpvc3MgdEbpR5-BdsQ7x34oSjidnKGhmQoGBrPiwLFVhoUcJrx5AzVA/exec",
+
+    // ==========================================
+    // Daily Sales Web App URL
+    // ==========================================
+
+    DAILY_SALES_API_URL:
+        "https://script.google.com/macros/s/AKfycbzZZ00AyOefjB5tsjYkmekY8dFfzbprCgILZXIZ2ecWOnDHF671uXI4p-Earx1s_T4p/exec",
 
     // ==========================================
     // Application
